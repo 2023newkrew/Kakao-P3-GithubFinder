4dcd738d5891e0c6d53ce37cf6b68a86f2ce2aaa
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-09 16:35:18 +0900

    chore: Setting up bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -37,8 +37,26 @@ module.exports = (env) => {
           ],
         },
         {
-          test: /\.s[ac]ss$/i,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
+          test: /\.(scss)$/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: () => [require('autoprefixer')],
+                },
+              },
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/,
