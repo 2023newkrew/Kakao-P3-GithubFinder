1f459ac8b9e65a3d09c814778f66702c3c438eeb
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-09 14:32:21 +0900

    chore: webpack 설정, README 작성

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Kakao-P3-GithubFinder
+
+## 개발
+
+### Quick Start
+
+```
+$ git clone https://github.com/b2bcodestatesOrg/Kakao-P3-GithubFinder.git
+
+$ cd Kakao-P3-GithubFinder
+
+$ npm install
+```
+
+### 로컬 서버 구동
+
+```
+$ npm run dev
+```
+
+### 빌드
+
+```
+$ npm run build
+```
---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    clean: process.env.NODE_ENV === "production",
+    assetModuleFilename: "[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+};
