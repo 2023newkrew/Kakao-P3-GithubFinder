2137387d45c2cb9d7bfa1b75d7615d55e2133c61
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-10 15:22:05 +0900

    feat: Footer 컴포넌트

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Header from "@/components/Header";
 import Search from "@/components/Search";
 import UserProfile from "@/components/UserProfile";
 import Repositories from "@/components/Repositories";
+import Footer from "@/components/Footer";
 
 class App extends Component {
   template() {
     return `
-      <header class="header navbar navbar-dark bg-primary"></header>
+      <header class="header navbar sticky-top navbar-dark bg-primary"></header>
       <main class="container d-flex flex-column gap-3 mt-3">
         <section class="search card"></section>
         <section class="user-profile card"></section>
         <section class="repositories"></section>
       </main>
+      <footer class="footer"></footer>
     `;
   }
 
@@ -21,6 +23,7 @@ class App extends Component {
     new Search(this.targetEl.querySelector(".search"));
     new UserProfile(this.targetEl.querySelector(".user-profile"));
     new Repositories(this.targetEl.querySelector(".repositories"));
+    new Footer(this.targetEl.querySelector(".footer"));
   }
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import Component from "@/core/Component";
+
+export default class Footer extends Component {
+  template() {
+    return `
+      <div class="py-3 text-center bg-light">GitHub Finder ©</div>
+    `;
+  }
+}
