a39ee66eb532ab9a52806b94da1517b7537351ea
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-10 14:45:23 +0900

    chore: Update eslint settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,84 +2,83 @@ const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = (env) => {
-  return {
-    mode: !!env.IS_DEV ? 'development' : 'production',
-    entry: path.resolve(__dirname, 'src/app.js'),
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].[contenthash].js',
-      clean: true,
-      assetModuleFilename: 'assets/[name][ext]',
+module.exports = (env) => ({
+  mode: env.IS_DEV ? 'development' : 'production',
+  entry: path.resolve(__dirname, 'src/app.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  devtool: env.IS_DEV ? 'inline-source-map' : false,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-    devtool: !!env.IS_DEV ? 'inline-source-map' : false,
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    open: true,
+    hot: false,
+    liveReload: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
       },
-      compress: true,
-      port: 3000,
-      open: true,
-      hot: false,
-      liveReload: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env'],
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                // eslint-disable-next-line global-require
+                plugins: () => [require('autoprefixer')],
               },
             },
-          ],
-        },
-        {
-          test: /\.(scss)$/,
-          use: [
-            {
-              loader: 'style-loader',
-            },
-            {
-              loader: 'css-loader',
-            },
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: () => [require('autoprefixer')],
-                },
-              },
-            },
-            {
-              loader: 'sass-loader',
-            },
-          ],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/,
-          type: 'asset/resource',
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'src/views/index.html',
-      }),
-      new CopyPlugin({
-        patterns: [{ from: 'src/assets/', to: 'assets/', noErrorOnMissing: true }],
-        options: {
-          concurrency: 100,
-        },
-      }),
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+      },
     ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/views/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'src/assets/', to: 'assets/', noErrorOnMissing: true }],
+      options: {
+        concurrency: 100,
       },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-  };
-};
+  },
+});
