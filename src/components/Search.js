78a410f6f85ad11d063a53da2cd0c70c87cf502f
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-11 15:05:46 +0900

    feat: 검색 실패 시 에러 메시지 출력

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,17 +7,29 @@ export default class Search extends Component {
       <form class="card-body">
         <div class="fs-2 fw-bold">깃허브 유저 검색</div>
         <div class="fs-4">저장소와 유저 프로필을 가져오기 위해 username을 검색해주세요.</div>
-        <input class="search__input w-100 mt-3 p-1 fs-5" placeholder="username" />
+        <input class="search__input w-100 mt-3 p-1 fs-5 form-control" placeholder="username" />
+        <div class="search__error invalid-feedback fs-6"></div>
       </form>
     `;
   }
 
   onMount() {
-    this.targetEl.addEventListener("submit", (event) => {
+    this.targetEl.addEventListener("submit", async (event) => {
       event.preventDefault();
       const username = this.targetEl.querySelector(".search__input").value;
 
-      UserInfoStore.fetchUserInfo(username);
+      try {
+        await UserInfoStore.fetchUserInfo(username);
+      } catch ({ message }) {
+        const inputEl = this.targetEl.querySelector(".search__input");
+        const errorEl = this.targetEl.querySelector(".search__error");
+
+        inputEl.classList.add("is-invalid");
+        errorEl.innerText = message;
+        inputEl.addEventListener("input", (event) => event.target.classList.remove("is-invalid"), {
+          once: true,
+        });
+      }
     });
   }
 }
