cd121166f9a689ad5e2b0f73359a48ea099e03d3
pyi7628 <pyi7628@gmail.com>
2023-01-12 16:03:45 +0900

    feat: api error 메시지 출력 로직 변경

--- src/scripts/common/API.js
+++ src/scripts/common/API.js
@@ -1,18 +1,22 @@
 import UI from "@/scripts/UI";
 
 export default class API {
-  constructor(rootEndpoint) {
+  constructor(rootEndpoint, errorMessage) {
     this.ui = new UI();
     this.rootEndpoint = rootEndpoint;
+    this.errorMessage = errorMessage;
   }
   async get(path, params) {
     try {
       const paramsStr = new URLSearchParams(params).toString();
       const response = await fetch(`${this.rootEndpoint}${path}?${paramsStr}`);
 
+      if (Object.keys(this.errorMessage).includes(response.status.toString())) {
+        throw new Error(this.errorMessage[response.status]);
+      }
       return { status: response.status, data: await response.json() };
     } catch (error) {
-      this.ui.alertError("통신에 문제가 생겼습니다!");
+      this.ui.alertError(error.message);
       return {};
     }
   }
--- src/scripts/controllers/SearchController.js
+++ src/scripts/controllers/SearchController.js
@@ -3,17 +3,18 @@ import UI from "@/scripts/UI";
 
 export default class SearchController {
   constructor() {
-    this.githubAPI = new API("https://api.github.com");
+    this.githubAPI = new API("https://api.github.com", {
+      403: "API 호출 횟수가 너무 많아요:( 잠시만 기다려주세요!",
+      404: "유저 정보를 찾을 수 없어요!",
+    });
     this.ui = new UI();
     this.bindEvents();
   }
 
   bindEvents() {
-    document
-      .getElementById("search__input")
-      .addEventListener("keydown", (e) => {
-        this.handleOnKeyDown(e);
-      });
+    document.getElementById("search__input").addEventListener("keydown", (e) => {
+      this.handleOnKeyDown(e);
+    });
   }
 
   async handleOnKeyDown({ code, target }) {
@@ -35,27 +36,18 @@ export default class SearchController {
   async getUserInfo(username) {
     const { status, data } = await this.githubAPI.get(`/users/${username}`);
     // 상수 처리하기!
-    if (status === 404) {
-      this.ui.alertError("유저 정보를 찾을 수 없어요!");
-      return;
-    } else if (status === 200) {
+    if (status === 200) {
       return data;
-    }
+    } else return;
   }
 
   async getUserRepository(username) {
-    const { status = 0, data } = await this.githubAPI.get(
-      `/users/${username}/repos`,
-      {
-        sort: "created",
-        per_page: 5,
-      }
-    );
-    if (status === 404) {
-      this.ui.alertError("유저 정보를 찾을 수 없어요!");
-      return;
-    } else if (status === 200) {
+    const { status = 0, data } = await this.githubAPI.get(`/users/${username}/repos`, {
+      sort: "created",
+      per_page: 5,
+    });
+    if (status === 200) {
       return data;
-    }
+    } else return;
   }
 }
