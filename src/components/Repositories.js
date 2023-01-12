942cda3e25037957fb6b25c40644ba12e8e78052
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-12 17:48:26 +0900

    feat: Repository 하위 컴포넌트 추가

--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,40 +1,35 @@
 import Component from "@core/Component";
 import UserInfoStore from "@stores/UserInfoStore";
+import Repository from "@components/Repository";
 
 export default class Repositories extends Component {
   template() {
-    if (!UserInfoStore.state.repositories.length) return "";
+    if (UserInfoStore.state.isLoading) return "";
 
     return `
       <div class="fs-3 fw-bold">Latest Repos</div>
       <ul class="repositories__repository-list list-unstyled d-flex flex-column gap-2 mt-2">
-        ${UserInfoStore.state.repositories
-          .map(
-            ({ html_url, name, stargazers_count, watchers_count, forks_count }) => `
-              <li class="repository card">
-                <div class="card-body row align-items-center">
-                  <div class="col-6">
-                    <a class="reposirory__name fs-5 text-decoration-none" href="${html_url}">${name}</a>
-                  </div>
-                  <div class="repository__stats col-6">
-                    <span class="stats__stars badge text-bg-primary fs-6 fw-normal">Stars: ${stargazers_count}</span>
-                    <span class="stats__watchers badge text-bg-secondary fs-6 fw-normal">Watchers: ${watchers_count}</span>
-                    <span class="stats__forks badge text-bg-success fs-6 fw-normal">Forks: ${forks_count}</span>
-                  </div>
-                </div>
-              </li>
-            `
-          )
-          .join("")}
+        <div class="py-2 text-center">No Public Repositories</div>
       </ul>
     `;
   }
 
   onMount() {
     UserInfoStore.subscribe(() => {
-      if (UserInfoStore.state.isLoading) return;
-
       this.render();
+    });
+
+    UserInfoStore.subscribe(() => {
+      const { isLoading, repositories } = UserInfoStore.state;
+
+      if (isLoading) return;
+      if (!repositories.length) return;
+
+      this.appendChildComponents(
+        this.targetEl.querySelector(".repositories__repository-list"),
+        Repository,
+        repositories.map((repository) => ({ repository }))
+      );
     });
   }
 }
